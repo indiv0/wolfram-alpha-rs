d9c994a5ad3901d399ba1bb69ab1a56b08df4ad6
Nikita Pekin <contact@nikitapek.in>
2017-11-20 20:45:49 -0500

    chore: allow compilation with warnings
    Allow the project to compile with warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-#![deny(missing_docs, non_camel_case_types, warnings)]
+#![deny(missing_docs, non_camel_case_types)]
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![cfg_attr(feature = "nightly", feature(custom_derive, proc_macro))]
